2ed94be7c37a08c0f985709174e6ca554967d408
Claudio Vilas <claudio.vilas@eldars.local>
2023-03-24 17:08:42 -0300

    sleuth-zipkin inicio de kafka

--- proyecto02/microservicios-spring-boot/order-service/src/main/java/com/caito/orderservice/service/impl/OrderServiceImpl.java
+++ proyecto02/microservicios-spring-boot/order-service/src/main/java/com/caito/orderservice/service/impl/OrderServiceImpl.java
@@ -1,15 +1,19 @@
 package com.caito.orderservice.service.impl;
 
+import brave.Span;
+import brave.Tracer;
 import com.caito.orderservice.dto.InventarioResponseDTO;
 import com.caito.orderservice.dto.OrderLinesItemsDTO;
 import com.caito.orderservice.dto.OrderRequestDTO;
 import com.caito.orderservice.entity.Order;
 import com.caito.orderservice.entity.OrderLinesItems;
+import com.caito.orderservice.event.OrderPlacedEvent;
 import com.caito.orderservice.repository.OrderRepository;
 import com.caito.orderservice.service.contract.OrderService;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -27,14 +31,18 @@ public class OrderServiceImpl implements OrderService {
     private OrderRepository orderRepository;
     @Autowired
     private WebClient.Builder webClient;
+    @Autowired
+    private Tracer tracer;
+    @Autowired
+    private KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
 
     @Override
-    @Transactional(readOnly = true)
+    @Transactional()
     @SneakyThrows
     public String placeOrder(OrderRequestDTO dto) {
         log.info("iniciando servicio crear pedidos");
-        Thread.sleep(10000);
+        //Thread.sleep(10000);
         Order order = new Order();
         log.info("end wait");
         order.setNumeroPedido(UUID.randomUUID().toString());
@@ -46,21 +54,30 @@ public class OrderServiceImpl implements OrderService {
         List<String> codigoSku = order.getOrderLinesItems().stream()
                         .map(OrderLinesItems::getCodigoSku)
                                 .collect(Collectors.toList());
-        InventarioResponseDTO[] inventarioArray = webClient.build().get()
-                        .uri("http://INVENTARIO-SERVICE/api/inventario", uriBuilder ->
-                             uriBuilder.queryParam("codigoSku", codigoSku).build())
-                        .retrieve()
-                        .bodyToMono(InventarioResponseDTO[].class)
-                        .block();
-        log.info("{} ", codigoSku);
-        boolean allProductsInStock = Arrays.stream(inventarioArray)
-                        .allMatch(InventarioResponseDTO::isInStock);
-        if (allProductsInStock){
-            orderRepository.save(order);
-            return "pedido realizado con exito";
-        }else {
-            throw new  IllegalArgumentException("El producto no esta en stock");
+        Span inventarioServiceLookup = tracer.nextSpan().name("InventarioServiceLookup");
+        try(Tracer.SpanInScope isLookUp = tracer.withSpanInScope(inventarioServiceLookup.start())) {
+            inventarioServiceLookup.tag("call", "inventario-service");
+            InventarioResponseDTO[] inventarioArray = webClient.build().get()
+                    .uri("http://INVENTARIO-SERVICE/api/inventario", uriBuilder ->
+                            uriBuilder.queryParam("codigoSku", codigoSku).build())
+                    .retrieve()
+                    .bodyToMono(InventarioResponseDTO[].class)
+                    .block();
+            log.info("{} ", codigoSku);
+            boolean allProductsInStock = Arrays.stream(inventarioArray)
+                    .allMatch(InventarioResponseDTO::isInStock);
+            if (allProductsInStock){
+                orderRepository.save(order);
+                kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(
+                        order.getNumeroPedido()));
+                return "pedido realizado con exito";
+            }else {
+                throw new  IllegalArgumentException("El producto no esta en stock");
+            }
+        }finally {
+            inventarioServiceLookup.flush();
         }
+
     }
 
     private OrderLinesItems mapToDto(OrderLinesItemsDTO orderLinesItemsDTO) {
